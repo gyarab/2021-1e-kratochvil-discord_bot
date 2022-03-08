1dbebc25a977fe8b7d31b936714860ff79fabffd
Matěj Kratochvíl <matej.kratochvil@student.gyarab.cz>
2022-03-08 19:21:21 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package Main;
+
+import Main.command.commands;
+import Main.listener.onJoinListener;
+import Main.listener.onLeaveListener;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder jda = JDABuilder.createDefault("OTEzODA5NTI4ODk2NzUzNjk2.YaD5cQ.9y19eKisyHOuiYKvjKANmiFUmWw");
+        jda.setActivity(Activity.watching("Made by Prach#9333"));
+        jda.setStatus(OnlineStatus.DO_NOT_DISTURB);
+        jda.addEventListeners(new commands(), new onJoinListener(), new onLeaveListener());
+        jda.setChunkingFilter(ChunkingFilter.ALL);
+        jda.setMemberCachePolicy(MemberCachePolicy.ALL);
+        jda.enableIntents(GatewayIntent.GUILD_MEMBERS);
+        jda.build();
+    }
+}
+
